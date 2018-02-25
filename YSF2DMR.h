c73171d99689e39e90adb761292f6a630f7a2e97
Andy CA6JAU <juribeparada@gmail.com>
2018-02-24 22:45:38 -0300

    Add copyright text

--- YSF2DMR.h
+++ YSF2DMR.h
@@ -1,6 +1,7 @@
 /*
 *   Copyright (C) 2016 by Jonathan Naylor G4KLX
 *   Copyright (C) 2018 by Andy Uribe CA6JAU
+*   Copyright (C) 2018 by Manuel Sanchez EA7EE
 *
 *   This program is free software; you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
