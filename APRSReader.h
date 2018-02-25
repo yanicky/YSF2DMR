9a2fd38dd0ebd2710509b5c427549643f0f59d54
Andy CA6JAU <juribeparada@gmail.com>
2018-02-25 11:25:19 -0300

    Formating fix

--- APRSReader.h
+++ APRSReader.h
@@ -1,6 +1,6 @@
 /*
 *   Copyright (C) 2016,2017 by Jonathan Naylor G4KLX
-*   Copyright (C) 2018 by Andy Uribe CA6JAU
+*   Copyright (C) 2018 by Manuel Sanchez EA7EE
 *
 *   This program is free software; you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
@@ -28,10 +28,10 @@
 
 class CAPRSReader : public CThread  {
 public:
-	CAPRSReader(std::string ApiKey,	int refres_time);
+	CAPRSReader(std::string ApiKey, int refres_time);
 	virtual ~CAPRSReader();
 
-	virtual void entry();	
+	virtual void entry();
 	
 	bool findCall(std::string cs, int *latitude, int *longitude);
     void formatGPS(unsigned char *buffer, int latitude, int longitude);
