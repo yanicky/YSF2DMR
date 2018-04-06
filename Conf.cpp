65997500dcce3e47b11163c0b1623582b9021d0c
Andy CA6JAU <juribeparada@gmail.com>
2018-04-06 14:10:48 -0300

    Remove quotes from config file values

--- Conf.cpp
+++ Conf.cpp
@@ -130,6 +130,16 @@ bool CConf::read()
       continue;
 
     char* value = ::strtok(NULL, "\r\n");
+    if (value == NULL)
+      continue;
+
+    // Remove quotes from the value
+    size_t len = ::strlen(value);
+    if (len > 1U && *value == '"' && value[len - 1U] == '"') {
+      value[len - 1U] = '\0';
+      value++;
+    }
+
 	if (section == SECTION_YSF_NETWORK) {
 		if (::strcmp(key, "Callsign") == 0) {
 			// Convert the callsign to upper case
